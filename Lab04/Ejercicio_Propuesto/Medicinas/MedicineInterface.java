d557a8d86a024ccfebb9a1183b8729d9f459ea84
Mario-Chura <mchurapum@unsa.edu.pe>
2024-05-23 01:24:42 -0500

    p1 MedicineInterface.java

---
+++ Lab04/Ejercicio_Propuesto/Medicinas/MedicineInterface.java
@@ -0,0 +1,10 @@
+package Medicinas;
+
+import java.rmi.Remote;
+
+/**
+ * Interfaz que define los métodos que pueden ser invocados de forma remota en objetos de tipo Medicine.
+ * Esta interfaz extiende la interfaz Remote para habilitar la comunicación remota.
+ * Contiene métodos para comprar medicinas, obtener el stock y imprimir detalles de la medicina.
+ * Estos métodos son implementados por la clase Medicine.
+ */
