6a8f8cc249b4a9348f3b3c104642ec47a19b6002
Mario-Chura <mchurapum@unsa.edu.pe>
2024-05-23 01:33:39 -0500

    p1 Stock.java

---
+++ Lab04/Ejercicio_Propuesto/Medicinas/Stock.java
@@ -0,0 +1,10 @@
+package Medicinas;
+
+import java.util.HashMap;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+/**
+ * Clase que implementa la interfaz StockInterface y proporciona la implementación de los métodos para administrar el inventario de medicinas.
+ * Extiende UnicastRemoteObject para permitir la comunicación remota.
+ */
