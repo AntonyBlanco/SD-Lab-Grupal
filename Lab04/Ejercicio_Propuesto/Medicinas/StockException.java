c1d10a18844f91cca59505bdb6be3d5179a3ee7e
Mario-Chura <mchurapum@unsa.edu.pe>
2024-05-23 01:35:34 -0500

    p1 StockException.java

---
+++ Lab04/Ejercicio_Propuesto/Medicinas/StockException.java
@@ -0,0 +1,6 @@
+package Medicinas;
+
+/**
+ * Clase que representa una excepción personalizada para manejar errores relacionados con el inventario de medicinas.
+ * Extiende la clase Exception para definir una excepción personalizada.
+ */
