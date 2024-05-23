a0aa844a9ca43c8da0f1846f9b08912fa7f39682
oliver092377 <95714352+oliver092377@users.noreply.github.com>
2024-05-22 21:49:17 -0500

    Comentarios a CalculatorClient.java

--- Lab04/Ejercicio_Resuelto/CalculatorClient.java
+++ Lab04/Ejercicio_Resuelto/CalculatorClient.java
@@ -1,36 +1,37 @@
 package Resuelto;
-
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.net.MalformedURLException;
 import java.rmi.NotBoundException;
+
 public class CalculatorClient {
     public static void main(String[] args) {
+        // Lee los argumentos de la línea de comandos
         int num1 = Integer.parseInt(args[0]);
         int num2 = Integer.parseInt(args[1]);
+
         try {
+            // Busca el objeto remoto registrado en el RMI Registry
             Calculator c = (Calculator) Naming.lookup("rmi://localhost/CalculatorService");
-            System.out.println( "The substraction of "+num1 +" and "+num2 +" is: "+ c.sub(num1, num2) );
-            System.out.println( "The addition of "+num1 +" and "+ num2 +"is: "+c.add(num1, num2) );
-            System.out.println( "The multiplication of "+num1 +" and "+num2 +" is: "+c.mul(num1, num2) );
-            System.out.println( "The division of "+num1 +" and "+ num2 +"is: "+c.div(num1, num2) );
-        }
-        catch (MalformedURLException murle) {
+
+            // Invoca los métodos remotos y muestra los resultados
+            System.out.println("The subtraction of " + num1 + " and " + num2 + " is: " + c.sub(num1, num2));
+            System.out.println("The addition of " + num1 + " and " + num2 + " is: " + c.add(num1, num2));
+            System.out.println("The multiplication of " + num1 + " and " + num2 + " is: " + c.mul(num1, num2));
+            System.out.println("The division of " + num1 + " and " + num2 + " is: " + c.div(num1, num2));
+        } catch (MalformedURLException murle) {
             System.out.println();
             System.out.println("MalformedURLException");
             System.out.println(murle);
-        }
-        catch (RemoteException re) {
+        } catch (RemoteException re) {
             System.out.println();
             System.out.println("RemoteException");
             System.out.println(re);
-        }
-        catch (NotBoundException nbe) {
+        } catch (NotBoundException nbe) {
             System.out.println();
             System.out.println("NotBoundException");
             System.out.println(nbe);
-        }
-        catch (java.lang.ArithmeticException ae) {
+        } catch (java.lang.ArithmeticException ae) {
             System.out.println();
             System.out.println("java.lang.ArithmeticException");
             System.out.println(ae);
