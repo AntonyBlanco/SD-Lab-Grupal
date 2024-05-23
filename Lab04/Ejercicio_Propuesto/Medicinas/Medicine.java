a20ccadd943315c45d5100e1fa9c90d0fe7ca74a
Mario-Chura <mchurapum@unsa.edu.pe>
2024-05-23 01:26:47 -0500

    p1 Medicine.java

---
+++ Lab04/Ejercicio_Propuesto/Medicinas/Medicine.java
@@ -0,0 +1,11 @@
+package Medicinas;
+
+import java.rmi.server.UnicastRemoteObject;
+
+/**
+ * Clase que representa una medicina en el proyecto.
+ * Esta clase implementa la interfaz MedicineInterface y proporciona la lógica para comprar y consultar medicinas.
+ * Se utiliza en el servidor para representar las medicinas disponibles en el inventario.
+ * 
+ * @author rventurar
+ */
