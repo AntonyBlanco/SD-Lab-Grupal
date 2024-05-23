324cc64fa92f67d4d253252fc2588066904cd978
Mario-Chura <mchurapum@unsa.edu.pe>
2024-05-23 01:35:56 -0500

    p2 StockException.java

--- Lab04/Ejercicio_Propuesto/Medicinas/StockException.java
+++ Lab04/Ejercicio_Propuesto/Medicinas/StockException.java
@@ -4,3 +4,13 @@ package Medicinas;
  * Clase que representa una excepción personalizada para manejar errores relacionados con el inventario de medicinas.
  * Extiende la clase Exception para definir una excepción personalizada.
  */
+public class StockException extends Exception {
+    
+    /**
+     * Constructor que recibe un mensaje de error y lo pasa al constructor de la superclase.
+     * @param msg El mensaje de error asociado con la excepción.
+     */
+    public StockException(String msg) {
+        super(msg);
+    }
+}
