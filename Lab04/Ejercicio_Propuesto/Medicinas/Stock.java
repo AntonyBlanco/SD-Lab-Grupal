7c4c2db3bc7e37097d9637b3a096153176cba7b3
Mario-Chura <mchurapum@unsa.edu.pe>
2024-05-23 01:34:09 -0500

    p2 Stock.java

--- Lab04/Ejercicio_Propuesto/Medicinas/Stock.java
+++ Lab04/Ejercicio_Propuesto/Medicinas/Stock.java
@@ -8,3 +8,18 @@ import java.rmi.server.UnicastRemoteObject;
  * Clase que implementa la interfaz StockInterface y proporciona la implementación de los métodos para administrar el inventario de medicinas.
  * Extiende UnicastRemoteObject para permitir la comunicación remota.
  */
+public class Stock extends UnicastRemoteObject implements StockInterface {
+    private HashMap<String, MedicineInterface> medicines = new HashMap<>(); // HashMap para almacenar las medicinas del inventario
+    
+    // Constructor por defecto
+    public Stock() throws RemoteException {
+        super();
+    }
+    
+    // Método para agregar una nueva medicina al inventario
+    public void addMedicine(String name, float price, int stock) throws Exception {
+        medicines.put(name, new Medicine(name, price, stock));
+    }
+    
+
+}
