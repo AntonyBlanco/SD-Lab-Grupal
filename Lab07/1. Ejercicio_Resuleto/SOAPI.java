16d226a7958a66788cfa348592ec05401cdc6a1b
Mario-Chura <mchurapum@unsa.edu.pe>
2024-06-06 13:30:04 -0500

    Update SOAPI.java

--- Lab07/1. Ejercicio_Resuleto/SOAPI.java
+++ Lab07/1. Ejercicio_Resuleto/SOAPI.java
@@ -1,14 +1,16 @@
+package es.rosamarfil.soap; // Define el paquete al que pertenece la interfaz SOAPI.
 
-package es.rosamarfil.soap;
-import java.util.List;
-import javax.jws.WebMethod;
-import javax.jws.WebService;
-import es.rosamarfil.model.User;
+import java.util.List; // Importa la interfaz List.
+import javax.jws.WebMethod; // Importa la anotación WebMethod para definir métodos de servicios web.
+import javax.jws.WebService; // Importa la anotación WebService para definir servicios web.
+import es.rosamarfil.model.User; // Importa la clase User del paquete es.rosamarfil.model.
 
-@WebService
+@WebService // Marca esta interfaz como un servicio web.
 public interface SOAPI {
-	@WebMethod
-	public List<User> getUsers();
-	@WebMethod
-	public void addUser(User user);
+    
+    @WebMethod // Indica que este método es una operación del servicio web.
+    public List<User> getUsers(); // Declaración del método para obtener una lista de usuarios.
+
+    @WebMethod // Indica que este método es una operación del servicio web.
+    public void addUser(User user); // Declaración del método para añadir un usuario.
 }
