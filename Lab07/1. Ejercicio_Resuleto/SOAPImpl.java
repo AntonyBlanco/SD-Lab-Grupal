57cdd7fea5f0b21a8a0366054f72f3e9ecf7fcae
Mario-Chura <mchurapum@unsa.edu.pe>
2024-06-06 13:42:01 -0500

    Update SOAPImpl.java

--- Lab07/1. Ejercicio_Resuleto/SOAPImpl.java
+++ Lab07/1. Ejercicio_Resuleto/SOAPImpl.java
@@ -1,18 +1,20 @@
-package es.rosamarfil.soap;
-import java.util.List;
-import javax.jws.WebService;
-import es.rosamarfil.model.User;
+package es.rosamarfil.soap; // Define el paquete al que pertenece la clase SOAPImpl.
 
-@WebService(endpointInterface = "es.rosamarfil.soap.SOAPI")
+import java.util.List; // Importa la interfaz List.
+import javax.jws.WebService; // Importa la anotación WebService.
+import es.rosamarfil.model.User; // Importa la clase User del paquete es.rosamarfil.model.
 
-public class SOAPImpl implements SOAPI{
-	@Override
-	public List<User> getUsers() {
-		return User.getUsers();
-	}
-	@Override
-	public void addUser(User user) {
-		User.getUsers().add(user);
-	}
+@WebService(endpointInterface = "es.rosamarfil.soap.SOAPI") // Especifica que esta clase implementa la interfaz SOAPI como un servicio web.
+public class SOAPImpl implements SOAPI { // Define la clase SOAPImpl que implementa la interfaz SOAPI.
+    
+    @Override // Anotación que indica que este método sobrescribe el método de la interfaz.
+    public List<User> getUsers() {
+        return User.users; // Devuelve la lista estática de usuarios de la clase User.
+    }
+
+    @Override // Anotación que indica que este método sobrescribe el método de la interfaz.
+    public void addUser(User user) {
+        User.users.add(user); // Añade un usuario a la lista estática de usuarios de la clase User.
+    }
 }
 
