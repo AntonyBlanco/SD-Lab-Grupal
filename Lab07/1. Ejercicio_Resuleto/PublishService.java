74fd1d0e317913a92880f7a6711c7c011c98a5fa
Mario-Chura <mchurapum@unsa.edu.pe>
2024-06-06 13:51:45 -0500

    Update PublishService.java

--- Lab07/1. Ejercicio_Resuleto/PublishService.java
+++ Lab07/1. Ejercicio_Resuleto/PublishService.java
@@ -1,14 +1,16 @@
+package es.rosamarfil; // Define el paquete al que pertenece la clase PublishServices.
 
-package es.rosamarfil;
-import javax.xml.ws.Endpoint;
-import es.rosamarfil.soap.SOAPImpl;
-public class PublishServices {
-	public static void main(String[] args) {
-		/*
-		 * Se publican los servicios a través de un servidor virtual.
-			El puerto puede ser cualquiera.
-			Un vez ejecutada la aplicación, se publica el contrato WSDL
-		 */
-		Endpoint.publish("http://localhost:1516/WS/Users", new SOAPImpl());
-	}
+import javax.xml.ws.Endpoint; // Importa la clase Endpoint para publicar servicios web.
+import es.rosamarfil.soap.SOAPImpl; // Importa la implementación del servicio web SOAPImpl.
+
+public class PublishServices { // Define la clase PublishServices.
+    public static void main(String[] args) { // Método principal que se ejecuta al iniciar la aplicación.
+        /*
+         * Se publican los servicios a través de un servidor virtual.
+         * El puerto puede ser cualquiera.
+         * Una vez ejecutada la aplicación, se publica el contrato WSDL.
+         */
+        Endpoint.publish("http://localhost:1516/WS/Users", new SOAPImpl()); // Publica el servicio web en la URL especificada.
+    }
 }
+
