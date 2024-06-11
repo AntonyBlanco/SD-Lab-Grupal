ef97937a0acc1dbbe1906457c308b0661b9931d3
Mario-Chura <mchurapum@unsa.edu.pe>
2024-06-11 14:20:07 -0500

    Update SOAPPublisher.java

--- Lab07/2. Ejercicio_Propuesto/SOAPPublisher.java
+++ Lab07/2. Ejercicio_Propuesto/SOAPPublisher.java
@@ -1,1 +1,10 @@
+package WSSOAP;
 
+import javax.xml.ws.Endpoint;
+
+public class SOAPPublisher {
+    public static void main(String[] args) {
+        Endpoint.publish("http://localhost:1516/WS/Products", new SOAPImpl());
+        System.out.println("Service is published!");
+    }
+}
