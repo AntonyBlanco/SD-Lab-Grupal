9566412bca1af96a187905b09e199d0209c6d517
Mario-Chura <mchurapum@unsa.edu.pe>
2024-06-11 13:56:31 -0500

    Update SOAPI.java

--- Lab07/2. Ejercicio_Propuesto/SOAPI.java
+++ Lab07/2. Ejercicio_Propuesto/SOAPI.java
@@ -1,1 +1,19 @@
+package WSSOAP;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import java.util.List;
+
+@WebService
+public interface SOAPI {
+    @WebMethod
+    List<Product> getProducts();
+
+    @WebMethod
+    Product getProductById(String id);
+
+    @WebMethod
+    String purchaseProduct(String id, int quantity);
+}
+
 
