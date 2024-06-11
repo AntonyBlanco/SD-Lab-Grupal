f776e8e71ac1efe1250ad4cc013a6b954da3dda6
Mario-Chura <mchurapum@unsa.edu.pe>
2024-06-11 14:21:22 -0500

    Update SOAPPublisher.java

--- Lab07/2. Ejercicio_Propuesto/SOAPPublisher.java
+++ Lab07/2. Ejercicio_Propuesto/SOAPPublisher.java
@@ -1,10 +1,15 @@
-package WSSOAP;
+package WSSOAP; 
 
-import javax.xml.ws.Endpoint;
+import javax.xml.ws.Endpoint; // Importa la clase Endpoint del paquete javax.xml.ws
 
+// Clase que publica el servicio web SOAP
 public class SOAPPublisher {
+    
+    // Método principal que se ejecuta al iniciar la aplicación
     public static void main(String[] args) {
+        // Publica el servicio web SOAP en la dirección especificada y con la implementación proporcionada
         Endpoint.publish("http://localhost:1516/WS/Products", new SOAPImpl());
+        // Imprime un mensaje indicando que el servicio ha sido publicado exitosamente
         System.out.println("Service is published!");
     }
 }
