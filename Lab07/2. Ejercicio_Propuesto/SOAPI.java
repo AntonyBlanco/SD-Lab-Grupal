1d25c33a198b995af73a94a0cf733b7c4bc0cab7
Mario-Chura <mchurapum@unsa.edu.pe>
2024-06-11 14:05:24 -0500

    Update SOAPI.java

--- Lab07/2. Ejercicio_Propuesto/SOAPI.java
+++ Lab07/2. Ejercicio_Propuesto/SOAPI.java
@@ -1,19 +1,23 @@
-package WSSOAP;
+package WSSOAP; 
 
-import javax.jws.WebMethod;
-import javax.jws.WebService;
-import java.util.List;
+import javax.jws.WebMethod; // Importa la anotación WebMethod
+import javax.jws.WebService; // Importa la anotación WebService
+import java.util.List; // Importa la clase List del paquete java.util
 
-@WebService
+//describe un conjunto de operaciones disponibles en un servicio web. 
+@WebService // Marca esta interfaz como un servicio web
 public interface SOAPI {
-    @WebMethod
+
+    // Método para obtener una lista de productos
+    @WebMethod 
     List<Product> getProducts();
 
-    @WebMethod
+    // Método para obtener un producto por su identificador
+    @WebMethod 
     Product getProductById(String id);
 
-    @WebMethod
+    // Método para comprar un producto
+    @WebMethod 
     String purchaseProduct(String id, int quantity);
 }
-
 
