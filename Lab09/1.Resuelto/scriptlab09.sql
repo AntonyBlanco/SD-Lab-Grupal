abe9d8f824d5a8d90eb9a3a798146d0054c99ba3
Mario-Chura <mchurapum@unsa.edu.pe>
2024-06-25 11:45:18 -0500

    Update scriptlab09.sql

--- Lab09/1.Resuelto/scriptlab09.sql
+++ Lab09/1.Resuelto/scriptlab09.sql
@@ -1,1 +1,19 @@
+-- Crear la base de datos
+CREATE DATABASE mydb3;
+
+-- Usar la base de datos
+USE mydb3;
+
+-- Estructura de tabla para la tabla 'mitabla'
+CREATE TABLE mitabla (
+    DNI VARCHAR(12) DEFAULT NULL,
+    correo VARCHAR(32) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Estructura de tabla para la tabla 'miotratabla'
+CREATE TABLE miotratabla (
+    nombre VARCHAR(20) DEFAULT NULL,
+    apellido VARCHAR(20) DEFAULT NULL,
+    edad INT(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
