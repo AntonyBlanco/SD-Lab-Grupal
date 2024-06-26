3d06d799820fafa015126fa1cb280405cd1911bb
Mario-Chura <mchurapum@unsa.edu.pe>
2024-06-25 21:55:48 -0500

    Update mydb4.sql

--- Lab09/2.Propuesto/mydb4.sql
+++ Lab09/2.Propuesto/mydb4.sql
@@ -1,1 +1,22 @@
+-- Crear la base de datos
+CREATE DATABASE mydb4;
+
+-- Usar la base de datos
+USE mydb4;
+
+CREATE TABLE Productos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    cantidad INT NOT NULL,
+    precio DECIMAL(10, 2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE Ventas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    producto_id INT NOT NULL,
+    cantidad INT NOT NULL,
+    total DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (producto_id) REFERENCES Productos(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
