febe1d7f1b39e9c206f22aee82d23a37df7277f1
Mario-Chura <mchurapum@unsa.edu.pe>
2024-06-25 23:05:20 -0500

    Update Database.java

--- Lab09/2.Propuesto/Database.java
+++ Lab09/2.Propuesto/Database.java
@@ -1,1 +1,28 @@
+package app;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+
+    private final static String bd = "mydb4";
+    private final static String login = "root";
+    private final static String password = "";
+    private final static String url = "jdbc:mysql://localhost:3306/" + bd;
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(url, login, password);
+            if (conn != null) {
+                System.out.println("Conectado a la base de datos [" + bd + "]");
+            }
+            return conn;
+        } catch (SQLException | ClassNotFoundException e) {
+            System.err.println(e.getMessage());
+        }
+        return null;
+    }
+}
 
